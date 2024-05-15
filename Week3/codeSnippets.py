529b2f45917f3ea93be41662ad9a2249263bea94
Prashant <50170332+PrashantSBasnet@users.noreply.github.com>
2024-05-15 11:59:30 +0545

    examples of global and nonlocal

--- Week3/codeSnippets.py
+++ Week3/codeSnippets.py
@@ -68,3 +68,85 @@ def myfunc():
     innerFunc()
     
 myfunc()
+
+----------------------------------------
+
+
+x = 20
+
+# Defining a function named add
+def add():
+    # Declaring x as a global variable inside the function
+    global x
+    # Incrementing the value of x by 23
+    x = x + 23
+    # Printing the updated value of x
+    print(x)
+
+# Calling the add function
+add()
+
+
+
+----------------------------------------
+
+y = 10
+def fun1():
+    print(y)
+    z = y +10
+    print (z)
+fun1()
+
+
+----------------------------------------
+
+def fun1():
+    x=10
+    def innerfun():
+        nonlocal x
+        x=x+10
+        print(x)
+    innerfun()
+
+fun1()
+
+----------------------------------------
+
+x =10
+def fun1():
+    def innerfun():
+        global x
+        x=x+10
+        print(x)
+    innerfun()
+
+fun1()
+
+----------------------------------------
+
+x = 20
+def add():
+    global x
+    x = x+23
+    print (x)
+    def inner():
+        global x
+        x=x*78
+        print(x)
+    inner()
+add()
+
+----------------------------------------
+
+x = 20
+def add():
+    global x
+    x = x+5
+    print (x) #Output:25
+    def inner():
+        global x # x=25
+        x=x+15 #x=25+15
+        print(x) #Output x=40
+    inner()
+add()
+print(x) #value has been changed x=40
